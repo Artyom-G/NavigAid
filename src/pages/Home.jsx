a8f82c86a2613ed32bc398a073d2e63d27e8e52c
Artyom-G <artyomgv05@gmail.com>
2025-05-03 05:21:36 -0400

    scss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import "./Home.scss";
 
 const Home = () => {
     return (
-        <iframe src="https://wheelmap.org?embedded=true&embedToken=12345&lat=52.5212&lon=13.4104" 
+        <iframe className="WheelMap" src="https://wheelmap.org?embedded=true&embedToken=12345&lat=52.5212&lon=13.4104" 
                 allow="geolocation"></iframe>        
     );
 }
